9d8de0d6dfeea3c1f039828eb5bf562294c240cf
Marcelo dos Santos Gonçalves <122677916+Ch1c4n0@users.noreply.github.com>
2026-05-22 23:52:26 -0300

    feat: add virtual network, subnet and public IP

---
+++ vnet.tf
@@ -0,0 +1,21 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-terraform-test"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "snet-internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "pip-vm-test"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
