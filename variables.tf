40dc631ea18c39bbfe71e744a7e99c0d5a021799
Marcelo dos Santos Gonçalves <122677916+Ch1c4n0@users.noreply.github.com>
2026-05-22 23:52:18 -0300

    feat: add variables

---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "resource_group_name" { default = "rg-terraform-test" }
+variable "location"            { default = "East US" }
+variable "vm_name"             { default = "vm-test-01" }
+variable "vm_size"             { default = "Standard_B1s" }
