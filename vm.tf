2c86ede2c0a7d422b17bf2c6857195f46943d59f
Marcelo dos Santos Gonçalves <122677916+Ch1c4n0@users.noreply.github.com>
2026-05-22 23:52:35 -0300

    feat: add network interface and linux virtual machine

---
+++ vm.tf
@@ -0,0 +1,36 @@
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-vm-test"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = var.vm_name
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = var.vm_size
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  admin_password                  = "P@ssw0rd1234!"
+  disable_password_authentication = false
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
